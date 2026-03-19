af186cdf57fe4a69c215284a8b2652bfd002deb9
Nandhini Gopal <Nandhini.Gopal@Triconinfotech.com>
2026-03-19 16:31:50 +0530

    NativeWind Insallation & Setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
+import "../global.css"
 
 export const unstable_settings = {
   anchor: '(tabs)',
--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { View, Text } from 'react-native';
 
 export default function ModalScreen() {
   return (
-    <View>
-      <Text>Hii</Text>
+    <View className={"bg-black-900 flex-1 justify-center items-center"}>
+      <Text className={"text-3xl font-bold text-center text-blue-500 bg-purple-500"}>Hii</Text>
     </View>
   );
 }
